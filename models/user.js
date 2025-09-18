77971bd524e42f040275056c0798918f4cd5189b
Timo <timokhan2@gmail.com>
2025-09-18 14:04:04 +0200

    first name and last name added to the schema

--- models/user.js
+++ models/user.js
@@ -2,6 +2,16 @@ import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema(
     {
+        firstName: {
+            type: String,
+            required: true,
+            trim: true
+        },
+        lastName: {
+            type: String,
+            required: true,
+            trim: true
+        },
         email: {
             type: String,
             required: true,
