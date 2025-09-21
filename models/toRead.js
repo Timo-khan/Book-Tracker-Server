428162c292390f08eef5dcc4c427559cd6a9ac54
Timo <timokhan2@gmail.com>
2025-09-21 15:26:17 +0200

    toRead schema updated

--- models/toRead.js
+++ models/toRead.js
@@ -1,9 +1,9 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 const toReadSchema = new mongoose.Schema(
     {
         google_id: { type: String, unique: true, required: true },
-        title: String,
+        title: { type: String, required: true },
         author: String,
         publisher: String,
         published_date: String,
@@ -11,14 +11,17 @@ const toReadSchema = new mongoose.Schema(
         thumbnail_url: String,
         isbn: String,
         category: String,
-        webReaderLink: String, 
-        pdfLink: String,        
-        epubLink: String 
+        webReaderLink: String,
+        pdfLink: String,
+        epubLink: String,
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',
+            required: true
+        }
     },
     { timestamps: true }
 );
-
-toReadSchema.index({ category: 1 });
 
 const ToRead = mongoose.model('ToRead', toReadSchema);
 export default ToRead;
