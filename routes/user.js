3a33ea0fec5b1a7c63fd2af0065dc049df539d79
Timo <timokhan2@gmail.com>
2025-09-21 13:57:31 +0200

    initial commit

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,5 @@
 import express from 'express';
-
+import { authMiddleware } from '../middleware/authMiddleware.js';
 import userControllers from '../controllers/user.js';
 
 const { register, login, logout } = userControllers;
@@ -11,4 +11,8 @@ router.post('/register', register);
 router.post('/login', login);
 router.post('/logout', logout);
 
+// new route for dashboard to fetch user info
+router.get('/me', authMiddleware, (req, res) => {
+    res.json({ user: req.user });
+});
 export default router;
