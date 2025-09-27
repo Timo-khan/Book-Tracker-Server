41fb39f39401e4691664ba6ebb64a3578a12c4c3
ivanaglavinic <ivanaglavinic8@gmail.com>
2025-09-27 10:20:14 +0200

    changed requirements for email

--- utils/validateEmail.js
+++ utils/validateEmail.js
@@ -1,5 +1,5 @@
 const validateEmail = (email) => {
-    const pattern = /^[a-z\d]+[\w.-]@[a-z\d]+[a-z\d-]\.[a-z]{2,63}$/i;
+    const pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return pattern.test(email);
 };
 
